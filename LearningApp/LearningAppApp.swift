3905ff1317eb6f899d35295b131aaa546d7ec5ea
Ramesh Rasaiyan <rasaiyan.ramesh@gmail.com>
2022-01-30 22:25:10 -0700

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Ramesh Rasaiyan on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
